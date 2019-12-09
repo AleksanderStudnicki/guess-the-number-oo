92ef5f75a4c246434204a3fe0e9c254b1e0bbb7d
alek.studnicki@gmail.com <alek.studnicki@gmail.com>
2019-12-09 14:47:36 +0100

    Added setUp method to RangeTest and extract Range object from existing tests to setUp method

--- src/test/java/app/studnicki/RangeTest.java
+++ src/test/java/app/studnicki/RangeTest.java
@@ -2,21 +2,28 @@ package app.studnicki;
 
 import app.studnicki.gtn.Range;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class RangeTest {
 
+    private Range range;
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("Setting up range...");
+        range = new Range(10, 100);
+        System.out.println("Set up range");
+    }
+
     @Test
     public void properlySetStartAndEndValueOfRange(){
-        Range range = new Range(10, 100);
-
         Assert.assertEquals(range.getStartValue(), Integer.valueOf(10));
         Assert.assertEquals(range.getEndValue(), Integer.valueOf(100));
     }
 
     @Test
     public void drawnRandomNumberInRange(){
-        Range range = new Range(10, 100);
         Integer random = range.getRandom();
 
         Assert.assertTrue(random >= range.getStartValue());
